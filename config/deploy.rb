6a63729ba50d25059441ec1a2cbb9c50b3deb57c
jd1386 <lee.jungdo@gmail.com>
2015-12-01 16:49:30 +0900

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+def red(str)
+  "\e[31m#{str}\e[0m"
+end
+
+# Figure out the name of the current local branch
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{red branch}"
+  branch
+end
+
+set :application, 'epiphany'
+set :repo_url, 'git@github.com:jd1386/epiphany.git'
+set :branch, current_git_branch
+set :deploy_to, '/home/deploy/epiphany'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+
+end
