64f171b027860d879357d11fcbdd2c5380ad4824
jd1386 <lee.jungdo@gmail.com>
2015-12-11 15:59:21 +0900

    add rbenv config

--- Capfile
+++ Capfile
@@ -24,8 +24,6 @@ require 'capistrano/bundler'
 require 'capistrano/rails'
 require 'capistrano/rbenv'
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.2.0'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,11 @@ set :application, 'epiphany'
 set :repo_url, 'git@github.com:jd1386/epiphany.git'
 set :branch, current_git_branch
 set :deploy_to, '/home/deploy/epiphany'
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
